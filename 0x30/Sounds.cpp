6ec55f44ea569587b7cb797e7d16ceb51bd31393
Rod <rod@badlydrawngames.com>
2020-04-19 17:18:54 +0100

    Tweak sounds.

--- 0x30/Sounds.cpp
+++ 0x30/Sounds.cpp
@@ -2,9 +2,9 @@
 
 
 Sounds::Sounds() :
-    blocksSwapping(je::LoadSound("assets/sounds/swish.wav")),
+    blocksSwapping(je::LoadSound("assets/sounds/swap.wav")),
     blocksLanding(je::LoadSound("assets/sounds/marble_click.wav")),
     blocksPopping(je::LoadSound("assets/sounds/pop.wav")),
-    menuSelect(je::LoadSound("assets/sounds/bloop.wav"))
+    menuSelect(je::LoadSound("assets/sounds/swap.wav"))
 {
 }
