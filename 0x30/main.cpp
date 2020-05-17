9bc5903f4f613fdbd744751803430568c406e257
Rod <rod@badlydrawngames.com>
2020-05-17 23:11:14 +0100

    WIP: minor tweaks

--- 0x30/main.cpp
+++ 0x30/main.cpp
@@ -216,7 +216,6 @@ static Game* theGame;
 static void main_loop(void)
 {
     // Update using: https://gafferongames.com/post/fix_your_timestep/
-//    glfwPollEvents();
     double now = je::GetTime();
     double delta = now - lastTime;
     if (delta >= 0.1)
