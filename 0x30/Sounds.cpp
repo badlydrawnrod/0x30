01e4127a843c6c4956bee28b6a9bc6b1b037ba80
Rod <rod@badlydrawngames.com>
2020-06-20 13:04:52 +0100

    WIP: just saving - probably doesn't even compile.

--- 0x30/Sounds.cpp
+++ 0x30/Sounds.cpp
@@ -12,12 +12,18 @@ template<typename T>
 class CrtpLoader
 {
 public:
+    void Download(const std::string& url, const std::string& filename);
     bool IsLoaded();
     void Load();
 
 private:
     std::future<void> loader_;
 };
+
+template<typename T>
+void CrtpLoader<T>::Download(const std::string& /*url*/, const std::string& /*filename*/)
+{
+}
 
 template<typename T>
 bool CrtpLoader<T>::IsLoaded()
@@ -28,37 +34,112 @@ bool CrtpLoader<T>::IsLoaded()
 template<typename T>
 void CrtpLoader<T>::Load()
 {
-    loader_ = std::async(&T::LoadFile, static_cast<T*>(this));
+    static_cast<T*>(this)->BeginLoad();
+    loader_ = std::async(&T::OnLoaded, static_cast<T*>(this));
 }
 
-class SingleFileLoader : public CrtpLoader<SingleFileLoader>
+#else
+
+#include "emscripten.h"
+
+template<typename T>
+class CrtpLoader
 {
 public:
-    explicit SingleFileLoader(const std::string& filename) : filename_{filename}
-    {
-    }
+    void OnFileDownloaded(const char* filename);
+    void Download(const std::string& url, const std::string& filename);
+    bool IsLoaded();
+    void Load();
 
-    void LoadFile();
+    static void OnSuccess(unsigned handle, void* userData, const char* filename);
+    static void OnFailed(unsigned handle, void*, int status);
+    static void OnProgress(unsigned handle, void*, int percent);
 
 private:
-    std::string filename_;
-    je::SoundBuffer sound_;
+    bool isLoaded_;
 };
 
-void SingleFileLoader::LoadFile()
+template<typename T>
+void CrtpLoader<T>::OnSuccess(unsigned handle, void* userData, const char* filename)
 {
-    sound_.TakeOwnership(je::LoadSound(filename_));
+    LOG("WGet2 downloaded: " << filename << " with handle = " << handle);
+    auto loader = reinterpret_cast<CrtpLoader<T>*>(userData);
+    loader->OnFileDownloaded(filename);
 }
+
+template<typename T>
+void CrtpLoader<T>::OnFailed(unsigned handle, void*, int status)
+{
+    LOG("WGet2 failed for handle " << handle << " with status code " << status);
+}
+
+template<typename T>
+void CrtpLoader<T>::OnProgress(unsigned handle, void*, int percent)
+{
+    LOG("WGet2 progress for handle " << handle << " = " << percent);
+}
+
+template <typename T>
+void CrtpLoader<T>::Download(const std::string& url, const std::string& filename)
+{
+    emscripten_async_wget2(url.c_str(), filename.c_str(), "GET", nullptr, this, OnSuccess, OnFailed, OnProgress);
+}
+
+#endif
+
+class SoundsLoader : public CrtpLoader<SoundsLoader>
+{
+
+public:
+    void BeginLoad();
+    void OnLoaded();
+
+    je::SoundBuffer blocksSwapping;
+    je::SoundBuffer blocksLanding;
+    je::SoundBuffer blocksPopping;
+    je::SoundBuffer menuSelect;
+
+    je::SoundBuffer musicMinuteWaltz;
+    je::SoundBuffer musicLAdieu;
+    je::SoundBuffer musicHallelujah;
+    je::SoundBuffer musicGymnopedie;
+};
+
+void SoundsLoader::BeginLoad()
+{
+    Download("sounds/swap.wav", "assets/sounds/swap.wav");
+    Download("sounds/marble_click.wav", "assets/sounds/marble_click.wav");
+    Download("sounds/pop.wav", "assets/sounds/pop.wav");
+    Download("sounds/swap.wav", "assets/sounds/swap.wav");
+    Download("music/minute.ogg", "assets/music/minute.ogg");
+    Download("music/adieu.ogg", "assets/music/adieu.ogg");
+    Download("music/hallelujah.ogg", "assets/music/hallelujah.ogg");
+    Download("music/gymnopedie1.ogg", "assets/music/gymnopedie1.ogg");
+}
+
+void SoundsLoader::OnLoaded()
+{
+    blocksSwapping.TakeOwnership(je::LoadSound("assets/sounds/swap.wav"));
+    blocksLanding.TakeOwnership(je::LoadSound("assets/sounds/marble_click.wav"));
+    blocksPopping.TakeOwnership(je::LoadSound("assets/sounds/pop.wav"));
+    menuSelect.TakeOwnership(je::LoadSound("assets/sounds/swap.wav"));
+    musicMinuteWaltz.TakeOwnership(je::LoadSound("assets/music/minute.ogg"));
+    musicLAdieu.TakeOwnership(je::LoadSound("assets/music/adieu.ogg"));
+    musicHallelujah.TakeOwnership(je::LoadSound("assets/music/hallelujah.ogg"));
+    musicGymnopedie.TakeOwnership(je::LoadSound("assets/music/gymnopedie1.ogg"));
+}
+
+#if !defined(__EMSCRIPTEN__)
 
 void Sounds::Load()
 {
-    LOG("Loading file");
-    SingleFileLoader sfl("assets/sounds/swap.wav");
-    sfl.Load();
-    while (!sfl.IsLoaded())
+    LOG("Loading files");
+    SoundsLoader soundsLoader;
+    soundsLoader.Load();
+    while (!soundsLoader.IsLoaded())
     {
     }
-    LOG("File loaded");
+    LOG("Files loaded");
 
     loader_ = std::async(&Sounds::LoaderTask, this);
 }
